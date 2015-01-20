1c9c9cb21612c8ef6a906288dd0a597bbb465c42
wenlai <wenchenglai@gmail.com>
2015-01-20 10:58:05 -0500

    working on user/account

---
+++ app/controllers/user/account.js
@@ -0,0 +1,71 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+    disabledImportFacebook: function () {
+        return Ember.isEmpty(this.get('facebookId'));
+    }.property('facebookId'),
+
+    actions: {
+        save: function () {
+            var self = this,
+                fromModel = this.get('model');
+
+            var onSuccess = function () {
+                self.send('closeModal');
+            };
+
+            var onFail = function (ret) {
+                if (ret.status === 200) {
+                    self.send('closeModal');
+                }
+                // deal with the failure here
+            };
+
+            fromModel.save().then(onSuccess, onFail);
+        },
+
+        importFromFacebook: function () {
+            var self = this,
+                fromModel = self.get('model');
+
+            FB.getLoginStatus(function (fbResponse) {
+                if (fbResponse.status === 'connected') {
+                    // if logged in before, the cookie will have this status
+                    Ember.run(function () {
+                        FB.api('/me?fields=id,name,address,email,birthday', function (fbUser) {
+                            if (!fromModel.get('email')) {
+                                fromModel.set('email', fbUser.email);
+                            }
+
+                            if (!fromModel.get('facebookId')) {
+                                fromModel.set('facebookId', fbUser.id);
+                            }
+                        });
+                    });
+                } else if (fbResponse.status === 'not_authorized') {
+                    // if facebook App setup is wrong, we could come here
+                    
+                } else {
+                    // status is unknown, then we must prompt with facebook login page
+                    FB.login(function (fbResponse) {
+                        if (fbResponse.authResponse) {
+                            Ember.run(function () {
+                                FB.api('/me?fields=id,name,address,email,birthday', function (fbUser) {
+                                    if (!fromModel.get('email')) {
+                                        fromModel.set('email', fbUser.email);
+                                    }
+
+                                    if (!fromModel.get('facebookId')) {
+                                        fromModel.set('facebookId', fbUser.id);
+                                    }
+                                });
+                            });
+                        } else {
+                            
+                        }
+                    });
+                }
+            });
+        }
+    }
+});
--- app/routes/user/account.js
+++ app/routes/user/account.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model: function() {
+        var self = this,
+            id = self.get('session.user.id'),
+            facebookId = self.get('session.user.facebookId');
+
+
+        if (id) {
+            return self.store.find('member', id);
+        } else if (facebookId) {
+            return self.store.find('member', facebookId);
+        } else {
+
+        }
+    },
+
+    afterModel: function(test1, test2) {
+        var a = test1;
+        var b = test2;
+        a.toString();
+        b.toString();
+    }
 });
