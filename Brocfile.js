f8cdc2536b426b869fd4b2b239076d11759b2c8f
wenlai <wenchenglai@gmail.com>
2015-02-05 20:21:22 -0500

    tip delete

--- Brocfile.js
+++ Brocfile.js
@@ -17,6 +17,10 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import('bower_components/jquery-ui/jquery-ui.js');
+app.import('bower_components/jquery-ui/ui/dialog.js');
+app.import('bower_components/jquery-ui/themes/smoothness/jquery-ui.css');
+
 app.import('bower_components/moment/moment.js');
 app.import('bower_components/ic-autocomplete/dist/globals/main.js');
 
---
+++ app/views/tip/my.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+export default Ember.View.extend({
+    didInsertElement: function () {
+        var self = this;
+        self.$("#dialog-confirm-delete").dialog({
+            autoOpen: false,
+            resizable: false,
+            height: 240,
+            modal: true,
+            buttons: {
+                "Delete": function () {
+                    var id = $(this).data('id');
+                    $(this).dialog("close");
+                    self.get('controller').send('delete', id);
+                },
+                Cancel: function () {
+                    $(this).dialog("close");
+                }
+            }
+        });
+    },
+    actions: {
+        openDeleteDialog: function (id) {
+            $("#dialog-confirm-delete").data('id', id).dialog("open");
+        }
+    }
+});
