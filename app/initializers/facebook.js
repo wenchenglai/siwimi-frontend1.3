a6497462680dae56b534dd27370d2bac7d0e695a
wenlai <wenchenglai@gmail.com>
2015-03-13 17:49:04 -0400

    fixed minor bug

--- app/initializers/facebook.js
+++ app/initializers/facebook.js
@@ -2,7 +2,7 @@ export function initialize(/* container, application */) {
     // application.inject('route', 'foo', 'service:foo');
     FB.init({
         appId: '343069969185068', // facebook Siwami Test AppID,
-        //appId: '290368724455193' // facebook Siwami Production AppID, 
+        //appId: '290368724455193', // facebook Siwami Production AppID, 
         cookie: true,  // enable cookies to allow the server to access 
         // the session
         xfbml: true,  // parse social plugins on this page
