b7eb9b046a84f3802138ef2556be0789d6e2b700
wenlai <wenchenglai@gmail.com>
2015-03-25 11:29:56 -0400

    fixed displayText

--- app/helpers/display-text.js
+++ app/helpers/display-text.js
@@ -1,9 +1,15 @@
 import Ember from 'ember';
 
 export function displayText(input, collection) {
-    var ret = "";
+    var ret = "",
+        item;
     if (input) {
-        ret = window._.find(collection, function(obj) { return obj.value === input; }).text;        
+        item = window._.find(collection, function(obj) { return obj.value === input; });
+        if (item) {
+            ret = item.text;
+        } else {
+            ret = input;
+        }  
     }
     return ret;
 }
