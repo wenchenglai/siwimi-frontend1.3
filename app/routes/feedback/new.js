e37c2a8068671529cd0f966e2f86a7bbd314a00e
Liming Shu <lshu@umich.edu>
2015-05-13 03:59:12 -0400

    added send action for feedback.new
    by liming

--- app/routes/feedback/new.js
+++ app/routes/feedback/new.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function () {
+    return this.store.createRecord('feedback');
+  },
+
+  actions: {
+    send: function() {
+      var self = this;
+      var model = self.get('controller.model');
+
+      model.save();
+    }
+  }
 });
