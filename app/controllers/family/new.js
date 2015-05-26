382b75b0e3c792ee992eb1add5e7359d8c51773c
Liming Shu <lshu@umich.edu>
2015-05-26 15:59:32 -0400

    minor fix

--- app/controllers/family/new.js
+++ app/controllers/family/new.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     disabled: function () {
-        return Ember.isEmpty(this.get('zipCode'));
-    }.property('zipCode'),
+        return Ember.isEmpty(this.get('model.zipCode'));
+    }.property('model.zipCode'),
 });
