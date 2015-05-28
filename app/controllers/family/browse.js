21c0d603a1e50bbdf2254b668f574a0eb5582424
Liming Shu <lshu@umich.edu>
2015-05-28 10:16:53 -0400

    minor fix

--- app/controllers/family/browse.js
+++ app/controllers/family/browse.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import CommonDataMixin from '../../mixins/common-data';
 
 export default Ember.Controller.extend(CommonDataMixin, {
-    allLanguages: ['Chinese (Mnadarin)', 'Chinese (Cantonese)', 'English', 'French', 'German', 'Indonesian', 'Japanese', 'Korean', 'Russian', 'Spanish'],
+    allLanguages: ['Chinese (Mandarin)', 'Chinese (Cantonese)', 'English', 'French', 'German', 'Indonesian', 'Japanese', 'Korean', 'Russian', 'Spanish'],
     allToys: ['Lego', 'Trains', 'Cars', 'Princess'],
     allNeeds: ['ADHD', 'Autism', 'Cerebral Palsy', 'Deafness/Hearing Loss', 'Down Syndrome', 'Learning Disability', 'Mental Retardation', 'Speech and Language Impairments', 'Visual Impairments'],
     toAge: 6,
--- app/controllers/member/edit.js
+++ app/controllers/member/edit.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import StatesDataMixin from '../../mixins/states-data';
 
 export default Ember.Controller.extend(StatesDataMixin, {
-    allLanguages: ['Chinese (Mnadarin)', 'Chinese (Cantonese)', 'English', 'French', 'German', 'Indonesian', 'Japanese', 'Korean', 'Russian', 'Spanish'],
+    allLanguages: ['Chinese (Mandarin)', 'Chinese (Cantonese)', 'English', 'French', 'German', 'Indonesian', 'Japanese', 'Korean', 'Russian', 'Spanish'],
     allRelationships: [
       "Daughter",
       "Son",
