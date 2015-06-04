1a0b390a821a77a0ee67a01c493432738f3f26d6
wenchenglai <wenchenglai@gmail.com>
2015-06-04 12:13:48 -0400

    active main menu tab highlighting color

--- app/views/application.js
+++ app/views/application.js
@@ -1,10 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-    isActive: function(request, response) {
-        var self = this;
-        debugger;
-        var a = self.get('controller.currentPath');
 
-    }.property('currentPath')
 });
