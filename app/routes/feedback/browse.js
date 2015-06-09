335a99765bba21523db26b56ec47099cfad5fd5f
wenlai <wenchenglai@gmail.com>
2015-06-09 16:30:12 -0400

    can delete feedback now

--- app/routes/feedback/browse.js
+++ app/routes/feedback/browse.js
@@ -4,5 +4,13 @@ export default Ember.Route.extend({
     model: function () {
         var self = this;
         return self.store.find('feedback');
+    },
+
+    actions: {
+        delete: function (id) {
+            this.store.find('feedback', id).then(function (record) {
+                record.destroyRecord();
+            });
+        }
     }
 });
