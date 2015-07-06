cac7cfe4daee4b3e824860f680a8678a81d3095e
Liming Shu <lshu@umich.edu>
2015-07-06 15:15:34 -0400

    try to convert view/dialog-confirm-delete to components/delete-confirm

---
+++ app/components/delete-confirm.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  attributeBindings: ['title'],
+
+  didInsertElement: function () {
+        var self = this,
+            controller = self.get('controller');
+
+        self.$("#dialog-confirm-delete").dialog({
+            autoOpen: false,
+            resizable: false,
+            appendTo: '#' + self.get('elementId'),
+            height: 240,
+            modal: true,
+            buttons: {
+                "Delete": function () {
+                    var id = self.$(this).data('id');
+                    self.$(this).dialog("close");
+                    controller.send('delete', id);
+                },
+                Cancel: function () {
+                    self.$(this).dialog("close");
+                }
+            }
+        });
+    },
+    actions: {
+        openDeleteDialog: function (id) {
+            var self = this;
+
+            self.$("#dialog-confirm-delete").data('id', id).dialog("open");
+        }
+    }
+});
