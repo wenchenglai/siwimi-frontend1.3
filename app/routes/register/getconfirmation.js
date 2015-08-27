093acb225c3cb5eb869e3b2814c1e289d64f4695
wenchenglai <wenchenglai@gmail.com>
2015-08-27 10:44:23 -0400

    fixed some login process stuff

--- app/routes/register/getconfirmation.js
+++ app/routes/register/getconfirmation.js
@@ -1,12 +1,16 @@
 import Ember from 'ember';
+import ENV from '../../config/environment';
 
 export default Ember.Route.extend({
     actions: {
         resend: function(id) {
-            Ember.$.ajax({
-                url: 'email/sendConfirmation?id=' + id,
-                type: 'GET'
-            });
+            var host = ENV.apiHost;
+
+            $.getJSON(host + "/email/sendConfirmation?id=" + id);
+            //Ember.$.ajax({
+            //    url: 'email/sendConfirmation?id=' + id,
+            //    type: 'GET'
+            //});
         }
     }
 });
