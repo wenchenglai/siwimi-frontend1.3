b56131f6a3efb45cfacb8ae23102c52817554eeb
wenchenglai <wenchenglai@gmail.com>
2015-09-17 15:38:55 -0400

    added alert for forgot password feature

--- app/routes/forgot-password.js
+++ app/routes/forgot-password.js
@@ -3,22 +3,30 @@ import Validators from '../mixins/validate-utility';
 
 export default Ember.Route.extend(Validators, {
 
-  actions: {
-      forgotPassword: function(email) {
-        var self = this;
-        var host = 'http://localhost:8080';
+    actions: {
+        forgotPassword: function (email) {
+            var self = this;
+            var host = 'http://localhost:8080';
 
-         if (Ember.isEmpty(email)) {
-            self.send('error', {name: 'Data Error', message: "Email cannot be empty."});
-            return;
+            if (Ember.isEmpty(email)) {
+                self.send('error', {name: 'Data Error', message: "Email cannot be empty."});
+                return;
+            }
+
+            if (self.validateEmail(email)) {
+                $.getJSON(host + "/email/forgetpassword?email=" + email);
+
+                self.send('showAlertBar', {
+                    title: "Reset Password",
+                    message: "We've sent you an email to reset your password.",
+                    type : "alert-info"}
+                );
+
+                
+
+            } else {
+                self.send('error', {name: 'Data Error', message: "Email is not in the right format"});
+            }
         }
-
-          if (self.validateEmail(email)) {
-             $.getJSON(host + "/email/forgetpassword?email=" + email);
-
-          } else {
-              self.send('error', { name: 'Data Error', message: "Email is not in the right format"});
-        }
-      }
     }
 });
